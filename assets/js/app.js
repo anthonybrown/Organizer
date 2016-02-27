56988851e6ef9ccf2e6e7ac26dd34b29f6d9f9bd
anthonybrown <tony@tonybrown.me>
2016-02-26 20:23:31 -0500

    render collection with fetch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,20 @@
 Organizer = {
 	initialize: function () {
-
 		var events = new Organizer.Events()
-		events.fetch()
-		events.reset([ { title: 'test1' }, { title: 'test2' }, { title: 'test3' } ])
-
 		var eventsList = new Organizer.EventsListView({
 			collection: events
 		})
-		eventsList.render()
+
+		events.fetch({
+			success: function() {
+				events.reset([ { title: 'Cather on the Rye' }, { title: 'To Kill a Mocking Bird' }, { title: 'On the Road Again' } ])
+				eventsList.render()
+			},
+
+			error: function() {
+				console.error('There was an error.')
+			}
+		})
 	}
 };
 
