6863c4f139f1d13f4f4634904a83698d83994768
anthonybrown <tony@tonybrown.me>
2016-02-27 19:50:32 -0500

    refactor

--- assets/js/views/events.js
+++ assets/js/views/events.js
@@ -1,10 +1,13 @@
 Organizer.EventsListView = Backbone.View.extend({
 	initialize: function() {
 		// bind our events
+		// listenTo makes an object listen for events on another object
 		this.listenTo(this.collection, 'reset' , this.render)
 		this.listenTo(this.collection, 'add'   , this.render)
 		this.listenTo(this.collection, 'remove', this.render)
-    // both listenTo and on do the same.
+		// to stop listeningTo an object
+		// this.stopListening(this.collection)
+    // both listenTo works on a specific object, and on works on the entire collection
 		//this.collection.on('reset' , this.render, this);
 		//this.collection.on('add'   , this.render, this);
 		//this.collection.on('remove', this.render, this);
@@ -36,6 +39,9 @@ Organizer.EventView = Backbone.View.extend({
 		//this.$el.html( Handlebars.templates['event'](this.model.toJSON()) );
 		// I'm using the full Handlebars lib for dev
 		var template = Handlebars.compile($ ( '#event-template' ).html() )
+		// we can ust this.model.get or this.model.set to get and set respectively
+		//console.log(this.model.get('author') + ' ' + this.model.get('title'));
+		// use the .toJSON method to get all the attributes
 		this.$el.html(template(this.model.toJSON()))
 		return this
 	},
